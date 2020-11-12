c4faae63655c2a917a8139a731af487547cef8b5
sparsell <hello@sparsell.com>
2020-11-12 13:58:50 -0500

    create root route and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :healthchecks
   resources :students
   resources :users
   resources :schools
+  root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
